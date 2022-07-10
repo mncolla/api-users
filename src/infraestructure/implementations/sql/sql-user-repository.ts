e17929cf9b048e90254b1eb8c6a9c2cafbfbe329
maxi-camonapp <maximiliano@camonapp.com>
2022-07-10 02:00:43 -0300

    aplico cambios de conexion de database a repo sql

--- src/infraestructure/implementations/sql/sql-user-repository.ts
+++ src/infraestructure/implementations/sql/sql-user-repository.ts
@@ -1,51 +1,48 @@
-import { UserModel } from "../../drivers/sql/models/user";
+import { User as UserModel } from "../../drivers/sql/models/user";
 import { User } from "../../../domain/entities/user";
 import { UserRepository } from "../../../domain/repositories/user-repository";
-import { Database } from '../../drivers/sql/database';
+import { AppDataSource } from '../../drivers/sql/database';
 
 
 export class SqlUserRepository implements UserRepository {
 
-    database = Database.getInstance()
-
     async getAll(): Promise<User[]> {
-        const repository = (await this.database).getRepository(UserModel)
+        const repository = AppDataSource.getRepository(UserModel)
         const allUsers = repository.find()
-
         return allUsers
     }
 
     async save(user: User): Promise<User> {
-        const repository = (await this.database).getRepository(UserModel)
+        console.log("user", user)
+        const repository = AppDataSource.getRepository(UserModel)
         repository.save(user)
         return user
     }
 
     async getByUsername(username: string): Promise<User | null> {
-        const repository = (await this.database).getRepository(UserModel)
+        const repository = AppDataSource.getRepository(UserModel)
         const user = await repository.findOneBy({ username: username as string });
-
         /* if (!user) {
             throw new Error("User does not exist");
         }
- */
+        */
         return user
     }
 
     async update(user: User): Promise<User> {
-        const repository = (await this.database).getRepository(UserModel)
+        const repository = AppDataSource.getRepository(UserModel)
         await repository.update(user.id, user);
 
         return user
     }
 
     async delete(id: string): Promise<void> {
-        const repository = (await this.database).getRepository(UserModel)
+        const repository = AppDataSource.getRepository(UserModel)
         await repository.delete(id);
     }
 
     async getById(id: string): Promise<User | null> {
-        const repository = (await this.database).getRepository(UserModel)
+        const repository = AppDataSource.getRepository(UserModel)
         const user = await repository.findOneBy({ id: id as string });
 
         if (!user) {
