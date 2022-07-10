4e4c33569aff0d12c418ec8cefd29520eec2c405
maxi-camonapp <maximiliano@camonapp.com>
2022-07-10 02:00:08 -0300

    conviero database a modulo

--- src/infraestructure/drivers/sql/database.ts
+++ src/infraestructure/drivers/sql/database.ts
@@ -1,32 +1,23 @@
+import 'dotenv/config'
 import { DataSource } from 'typeorm'
-import { UserModel } from './models/user'
+import { User } from './models/user'
 
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '3306'),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: true,
+    logging: false,
+    entities: [User],
+    subscribers: [],
+    migrations: [],
+})
 
-export class Database {
-
-    static instance: DataSource;
-
-    constructor() { }
-
-    static async getInstance() {
-        if (!Database.instance) {
-            Database.instance = new DataSource({
-                type: "mysql",
-                host: process.env.DB_HOST,
-                port: parseInt(process.env.DB_PORT || '3306'),
-                username: process.env.DB_USERNAME,
-                password: process.env.DB_PASSWORD,
-                database: process.env.DB_NAME,
-                synchronize: true,
-                logging: true,
-                entities: [UserModel],
-                subscribers: [],
-                migrations: [],
-            })
-        }
-
-        return Database.instance;
-    }
-
-
-}
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Connection stablished")
+    })
+    .catch((error) => console.log(error))
